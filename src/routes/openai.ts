8771d945aadbe9081b736c1f9c2cebbaaf5d61c2
Leo Li <leo4life@163.com>
2024-08-28 22:14:21 -0700

    json schema mode

--- src/routes/openai.ts
+++ src/routes/openai.ts
@@ -1,29 +1,57 @@
 import { FastifyInstance } from 'fastify';
 import OpenAI from 'openai';
+import axios from 'axios';
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     organization: process.env.OPENAI_ORG_ID || undefined,
 });
 
+const openaiApiKey = process.env.OPENAI_API_KEY;
+
 export default async function routes(fastify: FastifyInstance) {
     fastify.post('/openai/chat', async (request, reply) => {
-        const { model_name, messages, stop_seq } = request.body as {
+        // Using axios here instead of sdk to bypass needing to define json schema here.
+
+        const { model_name, messages, stop_seq, response_format } = request.body as {
             model_name: string;
-            messages: OpenAI.Chat.ChatCompletionMessageParam[];
+            messages: any[];
             stop_seq?: string[];
+            response_format?: {
+                type: string;
+                json_schema?: object;
+            };
         };
-        try {
-            const completion = await openai.chat.completions.create({
-                model: model_name || "gpt-3.5-turbo",
-                messages,
-                stop: stop_seq,
-            });
-            reply.send(completion.choices[0].message.content);
-        } catch (err) {
-            const error = err as Error;
-            reply.status(500).send(error.message);
+
+        const maxRetries = 5;
+        let attempt = 0;
+        let response;
+
+        while (attempt < maxRetries) {
+            try {
+                response = await axios.post('https://api.openai.com/v1/chat/completions', {
+                    model: model_name || "gpt-4o-mini",
+                    messages,
+                    stop: stop_seq,
+                    response_format,
+                }, {
+                    headers: {
+                        'Authorization': `Bearer ${openaiApiKey}`,
+                        'Content-Type': 'application/json'
+                    },
+                    timeout: 2000 // Set timeout to 1.5 seconds
+                });
+                break; // Exit loop if request is successful
+            } catch (err) {
+                attempt++;
+                if (attempt >= maxRetries) {
+                    reply.send("Connection to OpenAI service timed out.");
+                    return;
+                }
+            }
         }
+        // @ts-ignore
+        reply.send(response.data.choices[0].message.content);
     });
 
     fastify.post('/openai/embed', async (request, reply) => {
